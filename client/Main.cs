ef0da545d391d37630cdc4eb9e8f0add7fa29991
fatbo <fatbo@DESKTOP-DS7HSU7>
2021-12-22 16:40:23 -0500

    Add project files.

---
+++ client/Main.cs
@@ -0,0 +1,39 @@
+﻿using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client
+{
+    public class Main : BaseScript //
+    {
+        [Command("clearwp")]
+        public void ClearwpCommand(int source, List<object> args, string raw) 
+        {
+            TriggerEvent("chat:addMessage", new
+            {
+                color = new[] { 255, 0, 0 },
+                multiline = true,
+                args = new[] { "^8Your Weapons Have Been Cleared!" }
+            });
+
+            RemoveAllPedWeapons(PlayerPedId(), false); 
+        }
+        
+
+        [EventHandler("loadout")] 
+        public void LoadoutCommand(int source, List<object> args, string raw) 
+        {
+            var ped = PlayerPedId();
+            RemoveAllPedWeapons(ped, false);
+            GiveWeaponToPed(ped, (uint)GetHashKey("weapon_combatpistol"), 100, false, false);
+            GiveWeaponToPed(ped, (uint)GetHashKey("weapon_pumpshotgun"), 100, false, false);
+            GiveWeaponToPed(ped, (uint)GetHashKey("weapon_carbinerifle"), 100, false, false);
+            GiveWeaponToPed(ped, (uint)GetHashKey("weapon_stungun"), 100, false, false);
+            GiveWeaponToPed(ped, (uint)GetHashKey("weapon_nightstick"), 100, false, false);
+        } 
+    }
+}
---
+++ server/Main.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+namespace server
+{
+	public class Main : BaseScript
+	{
+		[Command("loadout")]
+		public void LoadoutCommand(int source, List<object> args, string raw)
+		{
+			var player = Players[source];
+			// true
+			if (IsPlayerAceAllowed(source.ToString(),"Viper.loadout"))
+			{
+				TriggerClientEvent(player, "chat:addMessage", new
+				{
+					color = new[] { 255, 0, 0 },
+					multiline = true,
+					args = new[] {"^3You have now have your loadout!" }
+				});
+
+				TriggerClientEvent(player, "loadout");
+			}
+			// false
+			else
+			{
+				TriggerClientEvent(player, "chat:addMessage", new
+				{
+					color = new[] { 255, 0, 0 },
+					multiline = true,
+					args = new[] {"^8You do not have permission for this command"}
+				});
+
+
+			}
+		}
+	}
+}
